2e7178feb404de2f674a93a7c6bf2a79ebe6ba2c
Hampus Nordin <nordin.hampus@gmail.com>
2011-01-28 19:32:00 +0100

    Added the dom object literal to the options var. Description will be added later.

--- sbname.js
+++ sbname.js
@@ -14,44 +14,69 @@
  * Default options:
  *		
  *		speedIn: 	The animation speed to use when the product name is animated in.
- *					Defaults to 400 (ms).
+ *					-Defaults to 400 (ms).
  *
  *		speedOut: 	The animation speed to use when the product number is animated out.
- *					Defaults to 500 (ms).
+ *					-Defaults to 500 (ms).
  *
  *		argsIn: 	Extra Arguments to animate In. To skip the opacity,
  *					pass an empty object to argsIn. ie $(selector).sbname({argsIn: {} });
- *					Defaults to { _currentOpacityOfObject_ }.
+ *					-Defaults to { _currentOpacityOfObject_ }.
  *
  *		argsOut: 	Extra Arguments to animate Out. To skip the opacity,
  *					do the same as above, but for argsOut.
- *					Defaults to {opacity:0}.
+ *					-Defaults to {opacity:0}.
+ *
+ *		dom: 		Desc. Coming soon...,
+ *					....
+ *					-Defaults to {outer: '', pNumber: '', pName: ''}.
  *
  *		
 */
 
 $.fn.sbname = function(options) {
 	
-	//Store jQuery selection into a variable. Not really necessary but I tend to prefer this when $(this) doesn't
-	//point to the incoming jQuery selection deeper down in the functions hierarchy.
-	var collection = this;
-	
 	//Check if a parameter for direction has ben passed and overwrite defaults with user inputs.
 	var o = (typeof options !== 'string' && typeof options !== 'undefined') ? $.extend({}, $.fn.sbname.defaults, options) : $.extend({}, $.fn.sbname.defaults);
 	
-		//Do stuff with each element if they have been initiated, else skip it.
+	//Set correct collection to loop through. If nothing has been passed. Use default, ie: whatever selection .sbname() was called with.
+	var collection =	(o.dom.outer != '' && typeof o.dom.outer == 'string') ? $(o.dom.outer) :
+						(o.dom.outer instanceof jQuery) ? o.dom.outer :
+						this;
+
+	//Make sure collection is a jQuery collection.
+	if (collection instanceof jQuery) {
+	
 		//Return the elements for 'chainability'.
 		return collection.each(function() {
-			var e = $(this);
-			var artnr = e.html();
+			
+			//Set the number element which holds the product number.
+			var pNumber	=	(o.dom.pNumber != '' && typeof o.dom.pNumber == 'string') ? $(this).find(o.dom.pNumber) :
+							(o.dom.pNumber instanceof jQuery) ? o.dom.pNumber :
+							$(this);
+			
+			//Should we get the product number from the value attribute or the innerhtml?
+			var pNumberType = (/^(?:area|textarea|input)$/i.test(pNumber[0].tagName)) ? 'value' : 'html';
+
+			//Set the name element which will get the product name if found.
+			var pName = 	(o.dom.pName != '' && typeof o.dom.pName == 'string') ? $(this).find(o.dom.pName) :
+							(o.dom.pName instanceof jQuery) ? o.dom.pName :
+							$(this);
+			
+			//Should we set the product name on the value attribute or in the innerhtml?
+			var pNameType	= (/^(?:area|textarea|input)$/i.test(pName[0].tagName)) ? 'value' : 'html';
+			
+			//Get the ArtNr/Product number.
+			var artnr =		(pNumberType == 'html') ? pNumber.html() : pNumber.attr('value');
 			
 			//Strip everything but the product number.
 			var regex = /^[\d]+/;
-			artnr = regex.exec(artnr);
+			var artnr = regex.exec(artnr);
 			
 			//If artnr is an integer, continue.
 			if (artnr == parseInt(artnr)) {
 				
+				//The heart of this plugin. I let Yahoo! do all the hard work :)
 				//YQL which gets the span-element with the product name from systembolaget.se.
 				var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fsystembolaget.se%2FSokDrycker%2FProdukt%3FVaruNr%3D"+artnr+"%22%20and%20xpath%3D'%2F%2Fspan%5B%40class%3D%22rubrikstor%22%5D%2Ftext()%20%7C%20%2F%2Fspan%5B%40class%3D%22rubrikstortunn%22%5D%2Ftext()'&format=json&callback=";
 				
@@ -70,19 +95,26 @@ $.fn.sbname = function(options) {
 						var name = $.trim(r[0]).replace(/[\s]{1,}/g,' ');
 						
 						//Set animation args.
-						var opac = {opacity: e.css('opacity')};
+						var opac = {opacity: pName.css('opacity')};
 						var aOut = $.extend({}, o.argsOut);
 						var aIn = $.extend({}, opac, o.argsIn);
 						
 						//Animate Out, Set text, Animate In.
-						e.animate(aOut,o.speedOut, function() {
-							e.html(name);
-							e.animate(aIn,o.speedIn);
+						pName.animate(aOut,o.speedOut, function() {
+							if (pNameType == 'html') {
+								pName.html(name);
+							} else {
+								pName.attr('value',name);
+							}
+							pName.animate(aIn,o.speedIn);
 						});
 					}
 				});
 			}
 		});
+	}
+	//Went wrong with the selection. Do nothing and pass the collection on for further chainability. 
+	return $(this);
 };
 
 //Default values for the function call.
@@ -98,7 +130,10 @@ $.fn.sbname.defaults = {
 	argsIn: {},
 	
 	//Extra Arguments to animate Out. To skip the opacity, do the same as above, but for argsOut.
-	argsOut: {opacity:0}
+	argsOut: {opacity:0},
+	
+	//Args... description coming in due time...
+	dom: {outer: '', pNumber: '', pName: ''}
 	
 };
 
