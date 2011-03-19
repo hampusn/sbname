2f98a528a56e1e391d374f59fb59cb02b75199ea
Hampus Nordin <nordin.hampus@gmail.com>
2011-03-19 17:02:50 +0100

    Added support for Web Storage.

--- jquery.sbname.js
+++ jquery.sbname.js
@@ -6,7 +6,7 @@
  * Get a product's name, by it's product number, from systembolaget.se (which is the website 
  * of the state-owned company/store for liqour & other alcoholic beverages in Sweden.)
  * 
- * Version 0.2.0
+ * Version 0.3.0
  *
  * Comming:
  * -General optimization
@@ -14,8 +14,7 @@
  * -Better documentation
  * 
  * New features (possibly):
- * -Support for storing collected names in a local json-formatted textfile.
- *  Should be faster and also minimizes the risk of hitting the ip-based api-limit of YQL.
+ * -?
  * 
  * by Hampus Nordin
  * http://hampusnordin.se/
@@ -119,6 +118,11 @@ $.fn.sbname = function(options) {
 
 	//Make sure the used collection is a jQuery collection.
 	if (this instanceof jQuery) {
+		
+		var useDB = (sbnameDB.hasSupport()) ? true : false;
+		
+		if (useDB)
+			var db = sbnameDB.init();
 				
 		//Return the elements for 'chainability'.
 		return this.each(function() {
@@ -155,46 +159,69 @@ $.fn.sbname = function(options) {
 		
 			//If artnr is an integer, continue.
 			if (artnr == parseInt(artnr)) {
-							
-				//Get the name.
-				$.get(YQLstring(artnr), function(data) {
 				
-					//If a name was found, continue.
-					if (data != '' && data.query.results.results.h1.span != null) {
-						
-						//Makes it shorter below. We don't need the rest anyways.
-						data = data.query.results.results.h1.span;
-						
-						//Select string with the name.
-						//Filter out the product number and also all whitespaces and random newlines which SB seems to like.
-						//d1 contains the name (span.rubrikstor).
-						//And d2 contains the extended part (span.rubrikstortunn).
-						var d1 = cleanName(data[0].content);
-						var d2 = (typeof data[1].content != 'undefined') ? cleanName(data[1].content) : '';
+				var YQL = false;
+				
+				//Should we use the local storage or YQL?
+				if (useDB) {
 					
-						var name = (typeof d2 == 'string' && d2 != '') ? d1 +' '+ d2 : d1;
-					
+					//Check if the product exists in the local database
+					var nameObj = sbnameDB.getItem(artnr);
+					if(nameObj) {
+						
 						//Format name according to o.nameFormat.
-						name = nameFormat(Array(d1,d2),o.nameFormat);
-					
-						//Set animation args.
-						var aOut = $.extend({}, o.argsOut);
-						var aIn = $.extend({}, {opacity: pName.css('opacity')}, o.argsIn);
-					
-						//Animate Out, Set text, Animate In.
-						pName.animate(aOut,o.speedOut, function() {
-							if (pNameType == 'html') {
-								pName.html(name);
-							} else {
-								pName.attr('value',name);
-							}
-							pName.animate(aIn,o.speedIn);
-						});
-					}else{
-						//Trigger Error since no name was found. Prolly bad product number.
-						if (typeof o.error === 'function') o.error(pName);
+						var name = nameFormat(Array(nameObj.name, nameObj.extended), o.nameFormat);
+						
+						//Animate
+						anim(pName, pNameType, o, name);
+					} else {
+						
+						//Since the product didn't exist in the database, use YQL.
+						YQL = true;
 					}
-				});
+				} else {
+					
+					//No support for local storage. Use YQL.
+					YQL = true;
+				}
+				
+				//Should we use YQL? 
+				if (YQL) {
+				
+					//Get the name.
+					$.get(YQLstring(artnr), function(data) {
+				
+						//If a name was found, continue.
+						if (data != '' && data.query.results.results.h1.span != null) {
+						
+							//Makes it shorter below. We don't need the rest anyways.
+							data = data.query.results.results.h1.span;
+						
+							//Select string with the name.
+							//Filter out the product number and also all whitespaces and random newlines which SB seems to like.
+							//d1 contains the name (span.rubrikstor).
+							//And d2 contains the extended part (span.rubrikstortunn).
+							var d1 = cleanName(data[0].content);
+							var d2 = (typeof data[1].content != 'undefined') ? cleanName(data[1].content) : '';
+						
+							//Store the name to the database, if local storage is supported.
+							if (useDB) {
+								sbnameDB.setItem(artnr, d1, d2);
+								sbnameDB.update();
+							}
+						
+							//Format name according to o.nameFormat.
+							//var name = (typeof d2 == 'string' && d2 != '') ? d1 +' '+ d2 : d1;
+							var name = nameFormat(Array(d1,d2),o.nameFormat);
+							
+							//Animate
+							anim(pName, pNameType, o, name);
+						}else{
+							//Trigger Error since no name was found. Prolly bad product number.
+							if (typeof o.error === 'function') o.error(pName);
+						}
+					});
+				}
 			}else{
 				//Trigger Error since the product nr prolly was bad.
 				if (typeof o.error === 'function') o.error(pName);
@@ -286,6 +313,23 @@ function nameFormat(name,format) {
 	return fName;
 }
 
+function anim(pName, pNameType, o, name) {
+	
+	//Set animation args.
+	var aOut = $.extend({}, o.argsOut);
+	var aIn = $.extend({}, {opacity: pName.css('opacity')}, o.argsIn);
+
+	//Animate Out, Set text, Animate In.
+	pName.animate(aOut, o.speedOut, function() {
+		if (pNameType == 'html') {
+			pName.html(name);
+		} else {
+			pName.attr('value',name);
+		}
+		pName.animate(aIn,o.speedIn);
+	});
+}
+
 //The heart of this plugin. I let Yahoo! do all the hard work :)
 //YQL gets the h1-element within the div that has the class 'beverageProperties'. (for some reason there are 2 h1-elements on the product page.)
 //This h1 contains the product name.
@@ -293,6 +337,103 @@ function YQLstring(artNr) {
 	return "http://query.yahooapis.com/v1/public/yql?q=use%20%22http%3A%2F%2Fyqlblog.net%2Fsamples%2Fdata.html.cssselect.xml%22%3B%20select%20*%20from%20data.html.cssselect%20where%20url%3D%22http%3A%2F%2Fsystembolaget.se%2FSok-dryck%2FDryck%2F%3FvaruNr%3D"+artNr+"%22%20and%20css%3D%22div.beverageProperties%20h1%22&format=json&callback="
 }
 
+var sbnameDB = {
+	
+	//This will be the key for the 'database' (json string) stored in localStorage
+	dbname: "sbnameDB",
+	
+	//This is the current version of the 'database' scheme
+	version: '0.1',
+	
+	//This array will contain all names
+	names: [],
+	
+	//Init local storage.
+	init: function() {
+		var db;
+		
+		//Check if a database doesn't already exists
+		if (!localStorage[this.dbname]) {
+			
+			//Store initial values / db scheme
+			db = {
+				"version": this.version,
+				"names": this.names
+			};
+			localStorage[this.dbname] = JSON.stringify(db);
+		} else {
+			
+			//Get the stored db and update this singleton
+			db = JSON.parse(localStorage[this.dbname]);
+			this.version = db.version;
+			this.names = db.names;
+		}
+		return db;
+	},
+	
+	//Check if browser supports local storage.
+	hasSupport: function() {
+		return 'localStorage' in window && window['localStorage'] !== null;
+	},
+	
+	//Check if an article already exist in the database
+	itemExists: function(artnr) {
+		
+		//Pointless to do anything if there aren't any names in the array.
+		if (this.names.length < 1)
+			return;
+			
+		for (var i = 0; i < this.names.length; i++) {
+			
+			//If a match was found, return the key.
+			if (this.names[i].artnr == artnr)
+				return i;
+		}
+		
+		return false;
+	},
+	
+	//Get an article based on its article number
+	getItem: function(artnr) {
+		if (this.names.length < 1)
+			return;
+			
+		for (var i = 0; i < this.names.length; i++) {
+			
+			//If a match was found, return it.
+			if (this.names[i].artnr == artnr)
+				return this.names[i];
+		}
+		
+		return false;
+	},
+	
+	//Store an article or update an existing based on the article number
+	setItem: function(artnr, name, extended) {
+		if (!this.names)
+			return;
+			
+		var i = this.itemExists(artnr);
+		if (typeof i == "number") {
+			this.names[i] = {"artnr": String(artnr), "name": name, "extended": extended};
+		} else {
+			this.names.push({"artnr": String(artnr), "name": name, "extended": extended});
+		}			
+	},
+	
+	//Update 'database'
+	update: function() {
+		var db = JSON.parse(localStorage[this.dbname]);
+		
+		//Doesn't the schemes match? Update.
+		if (db.version != this.version) {
+			//Update Scheme - later
+		}
+		
+		//Update database
+		localStorage[this.dbname] = JSON.stringify({"version": this.version, "names": this.names});
+	}
+};
 
 })(jQuery);
 
